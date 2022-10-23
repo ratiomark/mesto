a02587079557c48c0156574d794e67a39617c754
ratiomark <yanagae@gmail.com>
2022-10-23 20:09:28 +0300

    fix: add alt for images

--- scripts/index.js
+++ scripts/index.js
@@ -2,32 +2,26 @@ const INITIAL_CARDS = [
   {
     title: 'Spore Forest',
     link: 'https://i.pinimg.com/564x/40/4d/fa/404dfaa0aa9cdbb0ebd8e70bed2ae9ad.jpg',
-    alt: 'Картинка Spore Forest'
   },
   {
     title: 'Lair of Behemonths',
     link: 'https://i.pinimg.com/564x/91/8a/59/918a594b039b8da22538985978149b75.jpg',
-    alt: 'Картинка Lair of Behemonths'
   },
   {
     title: 'The Great Tree',
     link: 'https://i.pinimg.com/564x/68/5b/cb/685bcbccbc86257753133fea7a198f6b.jpg',
-    alt: 'Картинка The Great Tree'
   },
   {
     title: 'Mushroom Forest',
     link: 'https://i.pinimg.com/564x/0d/33/bb/0d33bb844a836176c20c0d85de7c2235.jpg',
-    alt: 'Картинка Mushroom Forest'
   },
   {
     title: 'Tree Village',
     link: 'https://i.pinimg.com/564x/50/1a/60/501a607d9ad6aae8fc55857413c46556.jpg',
-    alt: 'Картинка Tree Village'
   },
   {
     title: 'Dark Path',
     link: 'https://i.pinimg.com/564x/14/46/8e/14468e41e4dc9c320eea11ad06cd7c3d.jpg',
-    alt: 'Картинка Dark Path'
   }
 ];
 
@@ -37,7 +31,6 @@ cards.forEach((card, index) => addDataToCards(card, INITIAL_CARDS[index]))
 function addDataToCards(cardFromPage, dataForCard) {
   cardFromPage.querySelector('.card__image').src = dataForCard['link'];
   cardFromPage.querySelector('.card__title').textContent = dataForCard['title'];
-  cardFromPage.alt = dataForCard['alt']
 }
 
 const likeButtons = document.querySelectorAll('.card__like-button');
